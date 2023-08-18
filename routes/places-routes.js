ee1842a7c268bafd7586509b41fae85f356c1105
Skanda <sskanda98@gmail.com>
2023-08-19 01:39:08 +0530

    added places route

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,11 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const place = DUMMY_PLACES.find((p) => p.id === req.params.pid);
   console.log("Listenning");
-  res.json({
-    message: "wat?",
-  });
+  res.json(place);
 });
 
 module.exports = router;
