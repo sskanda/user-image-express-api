58cb67b3ab5fa9de386dcb8285572b1ef0f32139
Skanda <sskanda98@gmail.com>
2023-08-19 02:09:34 +0530

    error handeling

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ const app = express();
 
 app.use("/api/places", placesRoutes);
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) return next(error);
+  res.status(error.code || 500).send(error.message || "Unknown Error");
+});
+
 app.listen(4000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -17,14 +17,20 @@ const DUMMY_PLACES = [
 ];
 
 router.get("/:pid", (req, res, next) => {
+  const error = new Error("Cant find places");
   const place = DUMMY_PLACES.find((p) => p.id === req.params.pid);
   console.log("Listenning");
+  if (!place) {
+    throw error;
+  }
   res.json(place);
 });
 
 router.get("/user/:uid", (req, res, next) => {
+  const error = new Error("Cant find users");
   const place = DUMMY_PLACES.find((p) => p.creator === req.params.uid);
-  res.json(place);
+  if (!place) return next(error); //for asynchronous
+  else res.json(place);
 });
 
 module.exports = router;
